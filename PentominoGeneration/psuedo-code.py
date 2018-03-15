d4475caafbeaad6c2e85e83e6ca3edf2645c710d
Austin Born <austinborn212@gmail.com>
2018-03-15 18:18:27 -0500

    Adjusted Polyomino class to initialize first block

--- PentominoGeneration/psuedo-code.py
+++ PentominoGeneration/psuedo-code.py
@@ -1,17 +1,28 @@
-Function
-input: Number of units n
-Output: Visual display of all possible arrangements
-    2n-1 array of each potential shape with first unit in middle
+# Import related libraries
+from __future__ import print_function
+import random
+
+# Global Variables
+max_x = 20
+max_y = 10
+num_poly = 7
+
+# Class representing a given polyomino configuration
+class Polyomino:
+
+    # Constructor -- pass in number of blocks and list of block coordinates
+    def __init__(self, num_blocks):
+        self.num_blocks = num_blocks
+        self.array = [[0 for i in range(2*num_blocks-1)] for j in range(2*num_blocks-1)]
+        self.array[num_blocks-1][num_blocks-1] = 1
+        
+# Initialize polyomino
+poly = Polyomino(num_poly)
+for row in range(2*num_poly-1):
+    print(poly.array[row])
 
 
-#makeShape(n) takes in
-makeShape(n):
-    if n == 1:
-        baseShapes = [empty list]
-    baseShapes = makeShapes(n-1)
-    # 
 
-    
 #Function takes in 2-D coordinate array and returns list of available spaces around it
     # For each coordinate in shape
         # add each surrounding coordinate to list
@@ -20,26 +31,19 @@ makeShape(n):
     # Add all potential spaces for each existing coordinate,
         # Remove existing coordinates
 
-max_x = 10
-max_y = 12
-coor_array
+# max_x = 10
+# max_y = 12
+# coor_array = []
 
-available_spaces():
-    out_spaces = [['0' for x in range(max_x)] for y in range(max_y)]
-    for i in range(0, x_max):
-        for j in range(0, y_max):
-            if coor_array[i[j]] == 1:
-                
-                out_spaces[i[j]] = 1
+# available_spaces():
+#     out_spaces = [['0' for x in range(max_x)] for y in range(max_y)]
+#     for i in range(0, x_max):
+#         for j in range(0, y_max):
+#             if coor_array[i[j]] == 1:
+
+#                 out_spaces[i[j]] = 1
 
 
-#Function to flip y
-
-#Function to flip x
-
-#Function that takes in two shapes (coordinate lists) and checks if they are equal
-
-#Function to convert makeShape output into visual representation
 
 
 '''
@@ -52,3 +56,10 @@ Description: function takes in a number of arguments equal to arg1
 '''
 def functionName(arg1, arg2, arg3):
     return
+
+
+
+
+
+
+
