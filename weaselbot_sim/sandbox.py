ae6e5f9d45b86cb39e3c3c434df6eece5106349d
Thomas <tfd2@illinois.edu>
2018-04-10 16:19:24 -0500

    a work in progress

--- weaselbot_sim/sandbox.py
+++ weaselbot_sim/sandbox.py
@@ -6,29 +6,36 @@ from pygame.locals import *
 #
 #
 # Functions
+#calculates the likelihood of a collision occuring
 def collision_prop():
+    #Case 1: Hitting another WeaselBot
+    #case 2: Hitting a wall
 
-#
+#merges two objects into one
+def collision_merge(WeaselBot one, WeaselBot two):
 # classes, may add if needed
 # NOT DEFINED VARIABLES -- pseudocode to provide me variables to play with
 class WeaselBot(self, screen, x, y, length, hue):
     #First thing first is to initialize constructor
     #Want to get color, shape, movement speed
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.color = pygame.color(hue) #not done, figure out once in WiFi
         self.robot = rect(screen, self.color, Rect, width=2) #Bad line, edit
         self.move = 5 #assume initial velocity
+        self.collided = 0 #state of collision
         #This can be expanded/changed without affecting overall logic if robot shape changes
-        
-    #Define movement based on WeaselBot algorithm
-    def movement():
-        #algorithm for movement
 
+    #Define movement based on WeaselBot algorithm
+    def movement(self):
+        #algorithm for movement
+    def handling_collision(self):
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(500, 500)
     pygame.mouse.set_visible(0)
+    #fills in background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
@@ -40,7 +47,9 @@ def main():
     #create game clock
     clock = pygame.time.Clock()
     while 1:
-
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type ==
 
 
 
