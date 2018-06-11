7085e03e2c8ecbba0d7143a75bb17a4ae0191d8c
Austin Born <austinborn212@gmail.com>
2018-06-12 04:49:59 +0900

    Added speed variability and text instructions

--- weaselbot_sim/sandbox2-ajborn2.py
+++ weaselbot_sim/sandbox2-ajborn2.py
@@ -8,6 +8,8 @@ To run, type: "python sandbox2-ajborn2.py n m" where n = number of units, m = si
     Note: I recommend "python sandbox2-ajborn2.py 64 6"
 To quit, press "Esc"
 To print to .png, press "p"
+To change speed, press "1-9" (default=5)
+To pause, press "Space"
 
 TODO:
 - Deal with physics, erm, difficulties
@@ -26,6 +28,7 @@ import pygame
 import random
 import math
 import sys
+import time
 from pygame.locals import *
 from pygame.color import *
 import pymunk
@@ -34,6 +37,7 @@ from pymunk import Vec2d
 
 # Declare global colors
 # COLOR = (R, G, B) => 0-255 for each
+BLACK = (0,0,0)
 GRAY = (200, 200, 200)   
 
 # Global constants
@@ -43,7 +47,7 @@ proximityMargin = 2                 # extra margin for imperfectly-aligned shape
 dampingConstant = 0.99996           # Amount of velocity preserved per second (0.99996 = 0.004% loss per second)
 mass = 100                          # weaselball mass
 r = 15                              # weaselball radius from center to each vertex
-elasticityConstant = 0.1            # weaselball elasticity
+elasticityConstant = 0              # weaselball elasticity
 frictionConstant = 0.9              # weaselball friction
 dt = 60                             # clock speed multiplier (default=60)
 forceMagnitude = 0.0005             # magnitude of min and max range of random linear force applied to weaselballs
@@ -53,29 +57,34 @@ forcefreq = 50                      # frequency at which linear force is applied
 def addJoints(arbiter, space, data):
     # If shapes are within proximity and colliding at 2
     # points, add a PivotJoint to each point of contact
-    proximityValue = (2*math.cos(angle/2)*r)**2 + (math.sin(angle/2)*r)**2 + proximityMargin
+    maxProximityValue = (2*math.cos(angle/2)*r)**2 + (math.sin(angle/2)*r)**2
+    minProximityValue = (2*math.cos(angle/2)*r)**2
     bot1, bot2 = arbiter.shapes
     if (bot1, bot2) not in joints:
         joints[(bot1, bot2)] = False
+        joints[(bot2, bot1)] = False
     if joints[(bot1, bot2)] == False:
-        if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) < proximityValue:
-            #space.add(pymunk.PinJoint(bot1.body, bot2.body))
-            if len(arbiter.contact_point_set.points) == 2:
-                for c in arbiter.contact_point_set.points:
-                    space.add(pymunk.PivotJoint(bot1.body, bot2.body, c.point_a))
-                    # rest_length = math.sqrt((c.point_a[0]-c.point_b[0])**2 + (c.point_a[1]-c.point_b[1])**2)
-                    # stiffness = 1000
-                    # damping = 100
-                    # space.add(pymunk.DampedSpring(bot1.body, bot2.body, c.point_a+(10,10), c.point_b, rest_length, stiffness, damping))
-                    # Print statement for debugging
-                    print("joint added at:"+str(c.point_a)+"for "+str(bot1)+"and "+str(bot2))
-                joints[(bot1, bot2)] = True
+        if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) < maxProximityValue + proximityMargin:
+            if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) > minProximityValue + proximityMargin:
+                #space.add(pymunk.PinJoint(bot1.body, bot2.body))
+                if len(arbiter.contact_point_set.points) == 2:
+                    for c in arbiter.contact_point_set.points:
+                        space.add(pymunk.PivotJoint(bot1.body, bot2.body, c.point_a))
+                        # rest_length = proximityMargin
+                        # stiffness = 20000000
+                        # damping = 900000
+                        # space.add(pymunk.DampedSpring(bot1.body, bot2.body, c.point_a+(1,1), c.point_b, rest_length, stiffness, damping))
+                        # Print statement for debugging
+                        print("joint added at:"+str(c.point_a)+"for "+str(bot1)+"and "+str(bot2))
+                    joints[(bot1, bot2)] = True
+                    joints[(bot2, bot1)] = True
 
 # Main function body
 if __name__ == '__main__':
 
     # Initialize game
     pygame.init()
+    pygame.font.init()
     running = True
     weaselballNum = int(sys.argv[1])
     sides = int(sys.argv[2])
@@ -143,15 +152,36 @@ if __name__ == '__main__':
     # Game loop
     while running:
 
-        # Handle external events
+        # Handle external events (key presses)
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
             elif event.type == KEYDOWN and event.key == K_p:
-                pygame.image.save(screen, "bouncing_balls.png")
-        
+                localtime = str(time.strftime("%Y-%m-%d-%H-%M-%S."+str(pygame.time.get_ticks()%1000), time.localtime()))
+                pygame.image.save(screen, "weaselball_sim_"+localtime+".png")
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                dt = 0
+            elif event.type == KEYDOWN and event.key == K_1:
+                dt = 1
+            elif event.type == KEYDOWN and event.key == K_2:
+                dt = 4
+            elif event.type == KEYDOWN and event.key == K_3:
+                dt = 10
+            elif event.type == KEYDOWN and event.key == K_4:
+                dt = 30
+            elif event.type == KEYDOWN and event.key == K_5:
+                dt = 60
+            elif event.type == KEYDOWN and event.key == K_6:
+                dt = 100
+            elif event.type == KEYDOWN and event.key == K_7:
+                dt = 200
+            elif event.type == KEYDOWN and event.key == K_8:
+                dt = 300
+            elif event.type == KEYDOWN and event.key == K_9:
+                dt = 400
+
         # Clear screen
         screen.fill(GRAY)        
 
@@ -171,14 +201,21 @@ if __name__ == '__main__':
 
         # Necessary for drawing shapes
         space.debug_draw(draw_options)
+
+        # Instructions text
+        screen.blit(pygame.font.SysFont('arial', 10).render('Press \'Esc\' to quit', False, BLACK), (0,0))
+        screen.blit(pygame.font.SysFont('arial', 10).render('Press \'p\' to print to .png', False, BLACK), (0,10))
+        screen.blit(pygame.font.SysFont('arial', 10).render('Press \'1-9\' to change fps (default=5)', False, BLACK), (0,20))
+        screen.blit(pygame.font.SysFont('arial', 10).render('Press \'Space\' to pause', False, BLACK), (0,30))
         
         # Update screen image
         pygame.display.flip()
 
         # Increment clock
         clock.tick(dt)
+
         # Print statement for debugging
         print("time: "+str(pygame.time.get_ticks()))
 
         # Display frames/second as the display caption
-        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+        pygame.display.set_caption(str(sides)+"-Sided Weaselball Simulation")
