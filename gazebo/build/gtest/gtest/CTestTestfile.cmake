1b62179bdc94104226057ed5a19a12807b0080b7
Jbwasse2 <jbwasse2@illinois.edu>
2018-06-17 11:02:36 -0500

    Took out build and devel dir in git, added package.xml to description dir

--- gazebo/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/justin/Documents/bouncy/self-assembly/gazebo/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
