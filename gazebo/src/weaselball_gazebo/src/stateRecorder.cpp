fd5bcbc7014f641cc6374cd28f95f919cffd8e7c
Jbwasse2 <jbwasse2@illinois.edu>
2018-07-09 21:19:47 -0500

    Data collection works!

--- gazebo/src/weaselball_gazebo/src/stateRecorder.cpp
+++ gazebo/src/weaselball_gazebo/src/stateRecorder.cpp
@@ -25,11 +25,15 @@ namespace gazebo
     struct weaselballData
 	{
 		math::Vector3 position;
-		math::Vector3 linearVelocity;
-		math::Vector3 linearAcceleration;
-		math::Vector3 rotationalVelocity;
-		math::Vector3 rotationalAcceleration;
 		math::Vector3 rotationalDisplacement;//Yaw-Pitch-Roll
+		math::Vector3 linearVelocityWorld;
+		math::Vector3 linearAccelerationWorld;
+		math::Vector3 rotationalVelocityWorld;
+		math::Vector3 rotationalAccelerationWorld;
+		math::Vector3 linearVelocityRelative;
+		math::Vector3 linearAccelerationRelative;
+		math::Vector3 rotationalVelocityRelative;
+		math::Vector3 rotationalAccelerationRelative;
 
 	};
 }
@@ -70,21 +74,22 @@ namespace gazebo
 		return ret;
 
 	}
-	/*
-    struct weaselballData
+	void writeDataToCSV(weaselballData data, int id)
 	{
-		math::Vector3 position;
-		math::Vector3 linearVelocity;
-		math::Vector3 linearAcceleration;
-		math::Vector3 rotationalVelocity;
-		math::Vector3 rotationalAcceleration;
-		math::Vector3 rotationalDisplacement;//Yaw-Pitch-Roll
-
-	};*/
-	void writeDataToCSV(weaselballData data)
-	{
+		this->collectionFile << id << ", ";
 		//Create the header of the csv and init
-		this->collectionFile << data.position[0] << ", " << data.position[1] << ", " << data.position[2] << ", \n";	
+		this->collectionFile << data.position[0] << ", " << data.position[1] << ", " << data.position[2] << ", ";	
+		this->collectionFile << data.rotationalDisplacement[1] << ", " << data.rotationalDisplacement[1] << ", " << data.rotationalDisplacement[2] << ", ";	
+
+		this->collectionFile << data.linearVelocityWorld[0] << ", " << data.linearVelocityWorld[1] << ", " << data.linearVelocityWorld[2] << ", ";	
+		this->collectionFile << data.linearAccelerationWorld[0] << ", " << data.linearAccelerationWorld[1] << ", " << data.linearAccelerationWorld[2] << ", ";	
+		this->collectionFile << data.rotationalVelocityWorld[0] << ", " << data.rotationalVelocityWorld[1] << ", " << data.rotationalVelocityWorld[2] << ", ";	
+		this->collectionFile << data.rotationalAccelerationWorld[0] << ", " << data.rotationalAccelerationWorld[1] << ", " << data.rotationalAccelerationWorld[2] << ", ";	
+
+		this->collectionFile << data.linearVelocityRelative[0] << ", " << data.linearVelocityRelative[1] << ", " << data.linearVelocityRelative[2] << ", ";	
+		this->collectionFile << data.linearAccelerationRelative[0] << ", " << data.linearAccelerationRelative[1] << ", " << data.linearAccelerationRelative[2] << ", ";	
+		this->collectionFile << data.rotationalVelocityRelative[0] << ", " << data.rotationalVelocityRelative[1] << ", " << data.rotationalVelocityRelative[2] << ", ";	
+		this->collectionFile << data.rotationalAccelerationRelative[0] << ", " << data.rotationalAccelerationRelative[1] << ", " << data.rotationalAccelerationRelative[2] << ", \n";	
 	}
 	
 
@@ -102,7 +107,8 @@ namespace gazebo
         boost::bind( &StateCollector::Update, this ) );
 
 	  this->collectionFile.open (COLLECTION_PATH,std::ofstream::out);
-	  this->collectionFile << "Pos_x, Pos_y, Pos_z, LinearVelocity_X, LinearVelocity_Y, LinearVelocity_Z, LinearAcceleration_X, LinearAcceleration_Y, LinearAcceleration_Z, RotationalVelocity_X, RotationalVelocity_Y, RotationalVelocity_Z, RotationalAcceleration_X, RotationalAcceleration_Y, RotationalAcceleration_Z, Yaw, Pitch, Roll\n"; 
+	  this->collectionFile << "ID, Pos_x, Pos_y, Pos_z, Yaw, Pitch, Roll, Linear_Velocity_X_World, Linear_Velocity_Y_World, Linear_Velocity_Z_World, Linear_Acceleration_X_World, Linear_Acceleration_Y_World, Linear_Acceleration_Z_World, Rotational_Velocity_X_World, Rotational_Velocity_Y_World, Rotational_Velocity_Z_World, Rotational_Acceleration_X_World, Rotational_Acceleration_Y_World, Rotational_Acceleration_Z_World,Linear_Velocity_X_Relative, Linear_Velocity_Y_Relative, Linear_Velocity_Z_Relative, Linear_Acceleration_X_Relative, Linear_Acceleration_Y_Relative, Linear_Acceleration_Z_Relative_Relative, Rotational_Velocity_X_Relative, Rotational_Velocity_Y_Relative, Rotational_Velocity_Z_Relative, Rotational_Acceleration_X_Relative, Rotational_Acceleration_Y_Relative, Rotational_Acceleration_Z_Relative \n"; 
+
 	  std::cout << "State Collector has loaded!" << std::endl;
     }
 
@@ -127,21 +133,31 @@ namespace gazebo
 		for (auto weaselball : this->weaselballs)
 		{
 			weaselballData data;
+			std::string name = weaselball->GetName();
 			//Collect data for current state of robot i
 			physics::JointPtr shell = weaselball->GetJoint(SHELL_STRING);
-			getXYZCoordinates(weaselball, &data);
-			getLinearVelocity(weaselball, &data);
-			getAngularVelocity(weaselball, &data);
-			getAngularAcceleration(weaselball,  &data);
 
-			writeDataToCSV(data);
+			getXYZCoordinatesWorld(weaselball, &data);
+			getRotationalDisplacementWorld(weaselball, &data);
+			
+			getLinearVelocityWorld(weaselball, &data);
+			getAngularVelocityWorld(weaselball, &data);
+			getAngularAccelerationWorld(weaselball,  &data);
+			getLinearAccelerationWorld(weaselball, &data);
+
+			getLinearVelocityRelative(weaselball, &data);
+			getAngularVelocityRelative(weaselball, &data);
+			getAngularAccelerationRelative(weaselball,  &data);
+			getLinearAccelerationRelative(weaselball, &data);
+
+			writeDataToCSV(data, name.back());
 		    collection.push_back(data);	
 		}
 		this->collections.push_back(collection);
 
     }
 
-	void getXYZCoordinates(physics::ModelPtr weaselball, weaselballData* data)
+	void getXYZCoordinatesWorld(physics::ModelPtr weaselball, weaselballData* data)
 	{
 		math::Pose worldPose = weaselball->GetWorldPose();
 		math::Vector3 pos(0,0,0);
@@ -150,26 +166,54 @@ namespace gazebo
 
 	}
 
-	void getLinearVelocity(physics::ModelPtr weaselball, weaselballData* data)
+	void getRotationalDisplacementWorld(physics::ModelPtr weaselball, weaselballData* data)
 	{
-		data->linearVelocity = weaselball->GetWorldLinearVel();
+		math::Pose worldPose = weaselball->GetWorldPose();
+		math::Vector3 rotation(0,0,0);
+		rotation = worldPose.rot.GetAsEuler();
+		data->rotationalDisplacement = rotation;
+
+	}
+
+	void getLinearVelocityWorld(physics::ModelPtr weaselball, weaselballData* data)
+	{
+		data->linearVelocityWorld = weaselball->GetWorldLinearVel();
 	}
 	
-	void getAngularAcceleration(physics::ModelPtr weaselball, weaselballData* data)
+	void getAngularAccelerationWorld(physics::ModelPtr weaselball, weaselballData* data)
 	{
-		data->rotationalAcceleration = weaselball->GetWorldAngularAccel();
+		data->rotationalAccelerationWorld = weaselball->GetWorldAngularAccel();
 	}
 
-	void getAngularVelocity(physics::ModelPtr weaselball, weaselballData* data)
+	void getAngularVelocityWorld(physics::ModelPtr weaselball, weaselballData* data)
 	{
-		data->rotationalVelocity = weaselball->GetWorldAngularVel();
+		data->rotationalVelocityWorld = weaselball->GetWorldAngularVel();
 	}
 
-	void getLinearAcceleration(physics::ModelPtr weaselball, weaselballData* data)
+	void getLinearAccelerationWorld(physics::ModelPtr weaselball, weaselballData* data)
 	{
-		data->linearAcceleration = weaselball->GetWorldLinearAccel();
+		data->linearAccelerationWorld = weaselball->GetWorldLinearAccel();
 	}
 
+	void getLinearVelocityRelative(physics::ModelPtr weaselball, weaselballData* data)
+	{
+		data->linearVelocityRelative = weaselball->GetRelativeLinearVel();
+	}
+	
+	void getAngularAccelerationRelative(physics::ModelPtr weaselball, weaselballData* data)
+	{
+		data->rotationalAccelerationRelative = weaselball->GetRelativeAngularAccel();
+	}
+
+	void getAngularVelocityRelative(physics::ModelPtr weaselball, weaselballData* data)
+	{
+		data->rotationalVelocityRelative = weaselball->GetRelativeAngularVel();
+	}
+
+	void getLinearAccelerationRelative(physics::ModelPtr weaselball, weaselballData* data)
+	{
+		data->linearAccelerationRelative = weaselball->GetRelativeLinearAccel();
+	}
   };
 
   GZ_REGISTER_WORLD_PLUGIN( StateCollector )
