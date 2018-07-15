33cb8441a5cec79c38063b55d5bc3851348189b2
Austin Born <austinborn212@gmail.com>
2018-07-15 16:09:18 +0900

    Added new option to pymunk simulation for complete configurations (to be expanded)

--- weaselbot_sim/pymunk-simulation.py
+++ weaselbot_sim/pymunk-simulation.py
@@ -1,7 +1,8 @@
 '''
 This pygame simulation incorporates the pymunk library, which uses the Chipmunk
-physics engine. However, the joint connections and extreme angular velocities 
-have been difficult to deal with so far.
+physics engine. However, adding joints after initial start-up creates extreme
+behavior that I have had trouble debugging. Configurations made in the initialization
+of the simulation do not have this extreme behavior.
 
 To run, type: "python pymunk-simulation.py m n" where m = number of units, n = sides per unit
     Note: I recommend "python pymunk-simulation.py 64 6"
@@ -11,11 +12,14 @@ To change speed, press "1-9" (default=5)
 To pause, press "Space"
 
 TODO:
-- Deal with physics, erm, difficulties
-    - Currently using PivotJoint for connections, but with the randomly-added forces, 
-        this can lead to extreme angular velocities
-- Play with type of joint
-- Fix the way random forces are applied
+General:
+- Fix time counting
+- Fix force application frequency
+Option 1: 
+- Fix extreme behavior
+Option 2:
+- Automate joint creation
+- Simplify shape creation
 '''
 
 __version__ = "$Id:$"
@@ -40,16 +44,24 @@ GRAY = (200, 200, 200)
 
 # Global constants
 COLLTYPE_UNIT = 1                   # Collision type for weaselballs
-proximityMargin = 2                 # extra margin for imperfectly-aligned shapes to connect
-(screenw, screenh) = (400, 500)     # screen dimensions
-dampingConstant = 0.99996           # Amount of velocity preserved per second (0.99996 = 0.004% loss per second)
-mass = 100                          # weaselball mass
-r = 15                              # weaselball radius from center to each vertex
-elasticityConstant = 0              # weaselball elasticity
-frictionConstant = 0.9              # weaselball friction
+proximityMargin = 2                 # extra margin for imperfectly-aligned shapes to connect (default=2)
+(screenw, screenh) = (400, 500)     # screen dimensions (default=(400, 500))
+dampingConstant = 0.99996           # Amount of velocity preserved per second (default=0.99996 = 0.004% loss per second)
+mass = 100                          # weaselball mass (default=100)
+r = 15                              # shortest radius from center to each side (default=15)
+elasticityConstant = 1              # weaselball elasticity (default=0)
+frictionConstant = 0.9              # weaselball friction (default=0.9)
 dt = 60                             # clock speed multiplier (default=60)
-forceMagnitude = 0.0005             # magnitude of min and max range of random linear force applied to weaselballs
-forcefreq = 50                      # frequency at which linear force is applied
+forceMagnitude = 0.0005             # magnitude of min and max range of random linear force applied to weaselballs (default=0.0005)
+forcefreq = 50                      # frequency at which linear force is applied (default=50)
+
+# Returns False if in same group
+def groupCheck(arbiter, space, data):
+    bot1, bot2 = arbiter.shapes
+    if bot1.group == bot2.group:
+        return False
+    else:
+        return True
 
 # Collision callback function to constrain shapes
 def addJoints(arbiter, space, data):
@@ -58,18 +70,19 @@ def addJoints(arbiter, space, data):
     maxProximityValue = (2*math.cos(angle/2)*r)**2 + (math.sin(angle/2)*r)**2
     minProximityValue = (2*math.cos(angle/2)*r)**2
     bot1, bot2 = arbiter.shapes
-    if (bot1, bot2) not in joints:
-        joints[(bot1, bot2)] = False
-        joints[(bot2, bot1)] = False
-    if joints[(bot1, bot2)] == False:
-        if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) < maxProximityValue + proximityMargin:
-            if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) > minProximityValue + proximityMargin:
-                #space.add(pymunk.PinJoint(bot1.body, bot2.body))
-                if len(arbiter.contact_point_set.points) == 2:
-                    for c in arbiter.contact_point_set.points:
-                        space.add(pymunk.PivotJoint(bot1.body, bot2.body, c.point_a))
-                    joints[(bot1, bot2)] = True
-                    joints[(bot2, bot1)] = True
+    if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) < maxProximityValue + proximityMargin:
+        if ((bot1.body.position.x-bot2.body.position.x)**2 + (bot1.body.position.y-bot2.body.position.y)**2) >= minProximityValue:
+            if len(arbiter.contact_point_set.points) == 2:
+                space.add(pymunk.PivotJoint(bot1.body, bot2.body, arbiter.contact_point_set.points[0].point_a))
+                space.add(pymunk.GearJoint(bot1.body, bot2.body, bot2.body.angle - bot1.body.angle, 1))
+                print("joint: "+str(bot1)+', '+str(bot2))
+
+                # Make all units in new configuration the same group
+                groupToChange = bot2.group
+                for bot in space.shapes:
+                    if isinstance(bot, pymunk.Poly) and bot.body != None:
+                        if bot.group == groupToChange:
+                            bot.group = bot1.group
 
 # Main function body
 if __name__ == '__main__':
@@ -113,6 +126,7 @@ if __name__ == '__main__':
     for i in range(sides):
         vertices.append((r*math.cos((i+0.5)*angle),r*math.sin((i+0.5)*angle)))
 
+    '''# Option 1: Randomized balls that connect
     # Instantiate weaselball surface shapes and physics bodies
     # with random color and x- and y-positions
     for i in range(weaselballNum):
@@ -128,21 +142,55 @@ if __name__ == '__main__':
         shape.elasticity = elasticityConstant
         shape.friction = frictionConstant
         shape.color = color
+        shape.group = i
         space.add(body, shape)
+    # End of Option 1
+    '''
+
+    # Option 2: Given arrangement of balls
+    # Instantiate weaselball surface shapes and physics bodies
+    # with established x- and y-positions
+    xylist = [[screenw/2-r,screenh/2-r], [screenw/2+r, screenh/2-r], [screenw/2-r, screenh/2+r], [screenw/2+r, screenh/2+r]]
+    jointlist = [[screenw/2,screenh/2-r], [screenw/2-r,screenh/2], [screenw/2+r,screenh/2], [screenw/2,screenh/2+r]]
+    for i in range(4):
+        color = (random.randint(0,255), random.randint(0,255), random.randint(0,255), 1)
+        inertia = pymunk.moment_for_poly(mass, vertices)
+        body = pymunk.Body(mass, inertia)
+        body.position = xylist[i]
+        body.velocity = (0,0)
+        shape = pymunk.Poly(body, vertices)
+        shape.collision_type = COLLTYPE_UNIT
+        shape.elasticity = elasticityConstant
+        shape.friction = frictionConstant
+        shape.color = color
+        shape.group = 0
+        space.add(body, shape)
+    # Add joints
+    botlist = []
+    for bot in space.shapes:
+        if isinstance(bot, pymunk.Poly) and bot.body != None:
+            botlist.append(bot.body)
+    space.add(pymunk.PivotJoint(botlist[0], botlist[1], jointlist[0]))
+    space.add(pymunk.GearJoint(botlist[0], botlist[1], 0, 1))
+    space.add(pymunk.PivotJoint(botlist[0], botlist[2], jointlist[1]))
+    space.add(pymunk.GearJoint(botlist[0], botlist[2], 0, 1))
+    space.add(pymunk.PivotJoint(botlist[1], botlist[3], jointlist[2]))
+    space.add(pymunk.GearJoint(botlist[1], botlist[3], 0, 1))
+    space.add(pymunk.PivotJoint(botlist[2], botlist[3], jointlist[3]))
+    space.add(pymunk.GearJoint(botlist[2], botlist[3], 0, 1))
+    # End of Option 2
 
     # Instantiate collision handler
     ch = space.add_collision_handler(COLLTYPE_UNIT, COLLTYPE_UNIT)
     ch.data["surface"] = screen
+    ch.pre_solve = groupCheck
     ch.post_solve = addJoints
-
-    # Instantiate dictionary of joints
-    joints = {}
 
     # Counter for random motion
     counter = 0
 
     # Total time
-    time = 0
+    TotalTime = 0
 
     # Game loop
     while running:
@@ -176,9 +224,9 @@ if __name__ == '__main__':
             elif event.type == KEYDOWN and event.key == K_7:
                 dt = 200
             elif event.type == KEYDOWN and event.key == K_8:
-                dt = 300
-            elif event.type == KEYDOWN and event.key == K_9:
                 dt = 400
+            elif event.type == KEYDOWN and event.key == K_9:
+                dt = 800
 
         # Clear screen
         screen.fill(GRAY)        
@@ -192,6 +240,20 @@ if __name__ == '__main__':
         else:
             counter += 1
 
+        # Print impulse of each pinjoint - debugging
+        if dt > 0:
+            a = 0
+            maxhit = False
+            for joint in space.constraints:
+                if isinstance(joint, pymunk.PivotJoint):
+                    if joint.impulse/dt > 0.003:
+                        print("Joint "+str(a)+" force is: "+str(joint.impulse/dt))
+                        a += 1
+                        if joint.impulse/dt > 0.01:
+                            maxhit = True
+        if maxhit:
+            dt = 1
+
         # Update physics
         space.step(dt)
 
@@ -199,14 +261,14 @@ if __name__ == '__main__':
         space.debug_draw(draw_options)
 
         # Update total time
-        time += dt
+        TotalTime += dt
 
         # Instructions text
         screen.blit(pygame.font.SysFont('arial', 10).render('Press \'Esc\' to quit', False, BLACK), (0,0))
         screen.blit(pygame.font.SysFont('arial', 10).render('Press \'p\' to print to .png', False, BLACK), (0,10))
         screen.blit(pygame.font.SysFont('arial', 10).render('Press \'1-9\' to change fps (default=5)', False, BLACK), (0,20))
         screen.blit(pygame.font.SysFont('arial', 10).render('Press \'Space\' to pause', False, BLACK), (0,30))
-        screen.blit(pygame.font.SysFont('arial', 10).render("Total time: "+str(time), False, BLACK), (0,screenh-10))
+        screen.blit(pygame.font.SysFont('arial', 10).render("Total time: "+str(TotalTime), False, BLACK), (0,screenh-10))
         
         # Update screen image
         pygame.display.flip()
