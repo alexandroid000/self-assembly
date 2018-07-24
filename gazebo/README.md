8d03ee24227c8f6dd99f4fd7d45fe131754543d7
Jbwasse2 <jbwasse2@illinois.edu>
2018-07-23 20:45:32 -0500

    scripts, Created scripts folder, makde script for installing dependencies

--- gazebo/README.md
+++ gazebo/README.md
@@ -1,2 +1,19 @@
-Please install:
+Here are some high level scripts that I include for new users of ROS/Gazebo:
+
+install_dependencies.sh - This script install ROS, Gazebo, and other dependencies used by this repo
+
+create_workspace.sh - When using ROS your files should be in a "catkin workspace". This creates the workspace and moves everything to where it is suppsed to be. (TODO)
+
+run_4T.sh - This script launches gazebo and runs the simulator with the weaselball structure in a T formation (using 4 weaselballs)
+
+In other words if you've never used ROS/Gazebo before do the follow:
+
+1) run install_dependencies.sh
+2) run create_workspace.sh (TODO)
+3) run run_4T.sh
+
+Dependencies:
+(A script I made that can be used to install everything is available in the scripts directory)
+Ubuntu 16.04 (This is what I use, you may be able to use a different version/distro but I dont gaurantee stability of anything)
+ROS/Gazebo : See script for auto isntall (COMING SOON)
 https://github.com/PositronicsLab/Ravelin
---
+++ gazebo/scripts/install_dependencies.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#################################################
+#     Install Full ROS Kinetic for Ubuntu 16.04
+#################################################
+
+read -n1 -p "Do you need to install ROS? Enter (y) or (n)" doit
+echo
+if [[ $doit == "Y" || $doit == "y" ]]; then
+     # Setup your sources.list
+     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+     # Setup your keys
+     sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+     # Install Desktop-Full
+     sudo apt-get update
+     sudo apt-get install -y ros-kinetic-desktop-full
+
+     # Initialize rosdep
+     sudo rosdep init
+     rosdep update
+
+     # Setup bash Environment Loading
+     echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+     source ~/.bashrc
+     source /opt/ros/kinetic/setup.bash
+fi
+
+#################################
+#     Additional Packages
+################################
+read -n1 -p "Do you need to install the additional ROS packages? Enter (y) or (n)" doit
+echo
+if [[ $doit == "Y" || $doit == "y" ]]; then
+     # Joystick drivers for robot control
+     sudo apt install ros-kinetic-joy ros-kinetic-joystick-drivers ros-kinetic-joy-teleop ros-kinetic-teleop-twist-joy
+     # Drivers for robot navigation
+     sudo apt install ros-kinetic-gmapping ros-kinetic-amcl ros-kinetic-move-base ros-kinetic-map-server ros-kinetic-hector-gazebo*
+
+fi
+echo
+
+##############################
+#   Get Other Dependencies
+##############################
+read -n1 -p "Do you need to install the additional dependencies for this repo (Ravelin)? Enter (y) or (n)" doit
+echo
+if [[ $doit == "Y" || $doit == "y" ]]; then
+	yes | sudo add-apt-repository ppa:gwu-positronics/ravelin
+	sudo apt-get install ravelin
+fi
+
+
+##############################
+#   Gazebo install
+##############################
+read -n1 -p "Do you need to install Gazebo? Enter (y) or (n)" doit
+echo
+if [[ $doit == "Y" || $doit == "y" ]]; then
+	curl -sSL http://get.gazebosim.org | sh
+fi
+
+echo "Done!"
