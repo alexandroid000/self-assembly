289e233e947855385f184a54cea0502ac68b166e
Jbwasse2 <jbwasse2@illinois.edu>
2018-07-28 10:40:12 -0500

    dataAnalysis, added proof that we are oversampling the data

---
+++ dataAnalysis/develop/July-28-2018-jw-test_diagnol.py
@@ -0,0 +1,66 @@
+
+# coding: utf-8
+
+# In[27]:
+
+
+#The purpose of this test is to show that there are currently more samples for the diagnol than there are for the non diagnol parts of the matrix
+#This means that the data we record will often have the weaselball stay in the same state
+#I beleive this occurs because the simulator samples very quickly
+
+
+# In[28]:
+
+
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+get_ipython().magic(u'load_ext version_information')
+get_ipython().magic(u'version_information numpy, pandas, matplotlib')
+get_ipython().magic(u'version_information')
+fig_prefix = "../figures/2018-07-22-jw-weaselball-diagnol_"
+data_prefix = "../data/2018-07-22-jw-weaselball-diagnol_"
+
+
+# In[29]:
+
+
+df = pd.read_csv('../data/2018-07-22-jw-weaselball_analysis_translation_matrix_out.csv')
+df.head()
+
+
+# In[30]:
+
+
+sumDiagnol = 0
+sumNotDiagnol = 0
+
+for index, row in df.iterrows():
+    for i in range(row.size):
+        if(index == i):
+            sumDiagnol += df.iat[index,i]
+        else:
+            sumNotDiagnol += df.iat[index,i]
+
+
+# In[31]:
+
+
+sumDiagnolAveraged = sumDiagnol/(df.shape[0])
+sumNotDiagnolAveraged = sumDiagnol/(df.shape[0]**2 - df.shape[0])
+
+
+# In[32]:
+
+
+#This graphically shows that my hypothesis is true
+#As can be seen the average "Not Diagnol" probability is orders of magnitude smaller than the average "Diagnol" probability.
+data = pd.Series([sumDiagnolAveraged,sumNotDiagnolAveraged], index=['Diagnol', 'Not Diagnol'])
+x = ["Diagnol","Not Diagnol"]
+barplot = sns.barplot(x=x, y =np.log(data) )
+plot = barplot.get_figure()
+plot.savefig(fig_prefix + "diagnol_log_p.png")
+
