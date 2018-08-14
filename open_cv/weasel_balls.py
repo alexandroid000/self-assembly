64f2dd148a68110a2dc0bbee7d3d5896a764e00d
Alli Nilles <anilles137@gmail.com>
2018-08-14 16:18:27 -0600

    CoarseMarkov: docs and changes

--- open_cv/weasel_balls.py
+++ open_cv/weasel_balls.py
@@ -1,22 +1,25 @@
+#!/usr/bin/env python
+
 import sys
 import track_balls as tb
-import winding_numbers as wn
+#import winding_numbers as wn
 import cv2
 import os
+
 def main(in_vid):
     cap = cv2.VideoCapture(in_vid)
     vid_size = (int(cap.get(4)), int(cap.get(3)))
     save_name = os.path.splitext(in_vid)[0]
-    print "finding background..."
+    print("finding background...")
     back = tb.find_background(in_vid, vid_size, False, save_name)
-    print "tracking balls..."
+    print("tracking balls...")
     tb.track(in_vid, vid_size, back, True,save_name)
-    print "interpolating..."
+    print("interpolating...")
     tb.interpolate(save_name)
-    print "computing first order winding numbers..."
-    wn.first_order(True, save_name)
-    print "computing second order winding numbers..."
-    wn.second_order(True,save_name)
+    #print "computing first order winding numbers..."
+    #wn.first_order(True, save_name)
+    #print "computing second order winding numbers..."
+    #wn.second_order(True,save_name)
     return 1
     
 
