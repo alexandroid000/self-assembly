1c711b05dc5f4547f2e52fbae6c07dfb41f4d295
Alli Nilles <anilles137@gmail.com>
2018-08-29 12:32:19 -0500

    kronprod workinggit add test/test_kron.py

--- dataAnalysis/src/kronprod.py
+++ dataAnalysis/src/kronprod.py
@@ -13,72 +13,53 @@ DEBUG = False
 # TODO investigate LinearOperators for even moar fast
 #from scipy.sparse.linalg import LinearOperator
 
-def unwrap_mat(A):
-    return [elem for row in A for elem in row]
-
 class KronProd:
     def __init__(self, As, x):
         self.As = As
-        self.flat_A = np.concatenate([unwrap_mat(a) for a in self.As], axis=None)
+        self.flat_A = np.concatenate([a.flatten() for a in self.As], axis=None)
         self.nmat = len(self.As)
         self.n = [len(a) for a in self.As] # dimensions of factors
         self.N = reduce(mul, self.n, 1) # size of final vector y = A*x
-        self.Y = [0.0]*self.N
+        self.Y = np.empty(shape=self.N, dtype = np.float64)
         self.X = x
+        #self.Y = np.empty(shape=self.N, dtype = object)
+        #self.X = x
 
-    def contract(self, nk, mk):
+    def contract(self, nk, mk, ki):
         ktemp = 0
-        inic = 0
-        for i in range(1,nk+1):
+        inic = ki*(nk*nk)
+        for i in range(nk): # dim of matrix k
             J = 0
-            for s in range(1, int(mk+1)):
+            for s in range(int(mk)): # N / nk
                 I = inic
-                sum = 0
-                for t in range(1, nk+1):
+                #sum = ""
+                sum = 0.0
+                for t in range(nk): # dim of matrix k
+                    #sum = sum +"+"+ self.flat_A[I]+"*("+self.X[J]+")"
+                    sum = sum + self.flat_A[I]*self.X[J]
                     if DEBUG:
                         print("elem",I,"of",self.flat_A)
                         print("elem",J,"of",self.X)
-                    sum = sum + self.flat_A[I]*self.X[J]
+                        print("sum=", sum)
                     I = I + 1
-                    J = J+1
+                    J = J + 1
                 self.Y[ktemp] = sum
-                ktemp = ktemp+1
+                if DEBUG:
+                    print("setting element",ktemp,"of Y")
+                    print("Y is now", self.Y)
+                ktemp = ktemp + 1
             inic = I
-        for i in range(0,self.N):
-            self.X[i] = self.Y[i]
-
+            if DEBUG:
+                print("inic = ", inic)
+        np.copyto(self.X, self.Y)
 
     def tensorProd(self):
-        k = self.nmat-1
-        nk = self.n[k]
+        k = self.nmat
+        nk = self.n[k-1]
         mk = self.N/nk
-        while k >= 0:
+        for ki in range(k):
             if DEBUG:
-                print("IN CONTRACTION ",self.nmat - k)
+                print("IN CONTRACTION ",ki)
                 print("mk: ", mk)
-            k = k-1
-            mk = self.N/self.n[k]
-            self.contract(nk, mk)
-
-def test(n,p):
-    #r_As = [np.random.rand(p,p) for i in range(n)]
-    #As = [m/m.sum(axis=1)[:,None] for m in r_As] # normalize each row
-    As = [np.array([[1, 1], [1,1]]),
-          np.array([[1,1], [1,1]])]
-    #x = np.random.rand(p*n)
-    x = np.array([1,1,1,1])
-
-    print("efficient values")
-    # must reverse list for now
-    kp = KronProd(list(reversed(As)), x)
-    kp.tensorProd()
-    print(kp.Y)
-
-    print("correct values")
-    big_A = reduce(np.kron, As)
-    x = np.array([1,1,1,1])
-    print("A = ", big_A)
-    print("x = ", x)
-    big_y = np.matmul(big_A, x)
-    print("y = ", big_y)
-
+            mk = self.N/self.n[k-1-ki]
+            self.contract(nk, mk, ki)
