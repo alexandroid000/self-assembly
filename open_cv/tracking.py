b83b7a481faddcf397298f718e1ada0fa244c08d
Chris Horn <cchorn212@gmail.com>
2018-10-08 11:07:08 -0500

    Reorganizing code; fully functional single threading

--- open_cv/tracking.py
+++ open_cv/tracking.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
-import numpy as np 
+import numpy as np
 import cv2
 import argparse
 from collections import deque
 import sys
+
+min_ball_size = 10
+max_ball_size = 20
+
 
 
 def find_background(in_vid):
@@ -31,71 +35,67 @@ def find_background(in_vid):
 
 
 def track(in_vid):
+    #determine size and frame rate of input video
     cap = cv2.VideoCapture(in_vid)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    print (fps)
     capSize = (int(cap.get(3)), int(cap.get(4)))
-    min_ball_size = 10
-    max_ball_size = 20
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    background = find_background(in_vid)
-    if True:
-        fourcc = cv2.VideoWriter_fourcc(*'H264')
+
+    #save the output video
+    write = True
+    if write:
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')
         vout = cv2.VideoWriter()
         filename = 'tracking.mp4'
         success = vout.open(filename,fourcc,fps,capSize,True)
 
+    #open output file
     f = open("trajectory_data.txt", 'w')
 
-    num_ball = 0
-    accum_r = 0.0
-    r_count = 0.0
-    bd = 0 #balls detected?
-    while bd < 15:
-        success, frame = cap.read()
 
-        if success: #have a frame
-            ff = np.uint8((cv2.GaussianBlur(abs(frame-background), (3,3), 2)))
-            ff = cv2.cvtColor(ff, cv2.COLOR_BGR2GRAY) #HoughCircles requires greyscale image
-            circles = cv2.HoughCircles(ff, cv2.HOUGH_GRADIENT, 2, 17,param1=80,param2=25, minRadius = min_ball_size, maxRadius = max_ball_size)
-            if circles is not None:
-                circles = np.round(circles[0,:]).astype("int")
+    #find background image
+    background = find_background(in_vid)
 
-                if len(circles) == num_ball:
-                    bd = bd+1
-                else:
-                    num_ball = len(circles)
-                    bd = 0
-                for (x,y,r) in circles:
-                    accum_r = accum_r + r
-                    r_count = r_count + 1
-    avg_r = accum_r/r_count
+    #initial ball sizes
+    min_ball_size = 10
+    max_ball_size = 20
+
+
+    num_ball, avg_r = find_ball_count(cap, background)
+
     min_ball_size = int(avg_r) - 3
     max_ball_size = int(avg_r) + 3
 
+    #releasing and reopening to get the first frame
+    cap.release()
+    cap = cv2.VideoCapture(in_vid)
+
+    #trajectory path
     ball_data = []
+
+    #pull single frame and get initial locations for the balls
+    success, frame = cap.read()
+    circles = get_circles(frame, background)
     for (x,y,r) in circles:
         traj = deque()
         traj.append((x,y))
         ball_data.append(traj)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    max_frames = 4000
-
+    
+    #loop through video
     frame_count = 0
-    while(cap.isOpened() and frame_count < total_frames and frame_count < max_frames):
-        #print(frame_count)
+    while(cap.isOpened() and frame_count < total_frames):
         frame_count = frame_count+1
 
         success, frame = cap.read()
         if success: #have a frame
-            ff = np.uint8((cv2.GaussianBlur(abs(frame-background), (3,3), 2)))
-            ff = cv2.cvtColor(ff, cv2.COLOR_BGR2GRAY) #HoughCircles requires greyscale image
-            circles = cv2.HoughCircles(ff, cv2.HOUGH_GRADIENT, 2, 17,param1=80,param2=25, minRadius = min_ball_size, maxRadius = max_ball_size)
-            circles = np.round(circles[0,:]).astype("int")
+            circles = get_circles(frame, background)
+
             if len(circles) != num_ball:
                 continue
-            for (x,y,r) in circles:
-                cv2.circle(frame, (x,y),r,(0,255,0),4)
+            if write:
+                for (x,y,r) in circles:
+                    cv2.circle(frame, (x,y),r,(0,255,0),4)
             for i in range(num_ball):
                 dist = []
                 traj = ball_data[i]
@@ -110,15 +110,19 @@ def track(in_vid):
                 (x,y,r) = circles[idx]
                 circles[idx][2] = 0
                 traj.append((x,y))
-        vout.write(frame)
-        update_progress(frame_count/min((total_frames,max_frames)))
+        if write:
+            vout.write(frame)
+        update_progress(frame_count/total_frames)
 
+
+    #write data to text file
     while len(ball_data[0]):
         text = ""
         for traj in ball_data:
             text = text + str(traj.pop()) + ", "
         f.write(text + "\n")
 
+    #clean up
     vout.release()
     vout = None
     cap.release()
@@ -128,6 +132,51 @@ def track(in_vid):
 
 def update_progress(workdone):
     print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(workdone * 50), workdone*100), end="", flush=True)
+
+def find_ball_count(cap, background):
+    #finding average ball radius to improve detection
+    accum_r = 0.0
+    r_count = 0.0 
+
+    bd = 0 #balls detected in given frame (used to find number of balls)
+    num_ball = 0
+
+
+    while bd < 15:
+        success, frame = cap.read()
+
+        if success: #have a frame
+
+            #Frame preprocessing
+            ff = np.uint8((cv2.GaussianBlur(abs(frame-background), (3,3), 2))) #remove background and remove high frequency noise
+            ff = cv2.cvtColor(ff, cv2.COLOR_BGR2GRAY) #HoughCircles requires greyscale image
+
+            #detect all circles in frame
+            circles = cv2.HoughCircles(ff, cv2.HOUGH_GRADIENT, 2, 17,param1=80,param2=25, minRadius = min_ball_size, maxRadius = max_ball_size)
+            if circles is not None:
+                circles = np.round(circles[0,:]).astype("int")
+
+                if len(circles) == num_ball:
+                    bd = bd+1
+                else:
+                    num_ball = len(circles)
+                    bd = 0
+                for (x,y,r) in circles:
+                    accum_r = accum_r + r
+                    r_count = r_count + 1
+    avg_r = accum_r/r_count
+
+    return num_ball, avg_r
+
+
+
+def get_circles(frame, background):
+    ff = np.uint8((cv2.GaussianBlur(abs(frame-background), (3,3), 2)))
+    ff = cv2.cvtColor(ff, cv2.COLOR_BGR2GRAY) #HoughCircles requires greyscale image
+    circles = cv2.HoughCircles(ff, cv2.HOUGH_GRADIENT, 2, 17,param1=80,param2=25, minRadius = min_ball_size, maxRadius = max_ball_size)
+    circles = np.round(circles[0,:]).astype("int")
+    return circles
+
     
 
 
