dd7cfb9c0c8a37f26d0102f84e58d4bfde32da6f
Chris Horn <cchorn212@gmail.com>
2018-10-19 11:00:54 -0500

    ReadMe for weasel_balls script

--- open_cv/tracking.py
+++ open_cv/tracking.py
@@ -43,7 +43,7 @@ def track(in_vid):
 
 
     #save the output video
-    write = True
+    write = Falsea
     if write:
         fourcc = cv2.VideoWriter_fourcc(*'avc1')
         vout = cv2.VideoWriter()
