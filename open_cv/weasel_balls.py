55bf866fad22532d489f2f9ecb14e7c6c87129cd
Chris Horn <cchorn212@gmail.com>
2018-11-16 06:05:07 -0600

    Expanded help information in parser

--- open_cv/weasel_balls.py
+++ open_cv/weasel_balls.py
@@ -16,29 +16,37 @@ def main():
     parser = argparse.ArgumentParser(prog='Ball trajectory detecter', description='Process some weaselball video data')
     
     parser.add_argument('locs', 
+    help='Location of target file(s) -- file(s) for regular mode, directory(s) for batch mode',
     metavar='locs', 
     type=str, 
     nargs='+')
 
     parser.add_argument('-b', '--batch', 
+    help='Enable directory input (default: off)',
     action='store_true', 
     default='False',
     dest='batch_mode')
 
 
     parser.add_argument('--cores', 
+    help='Number of cores (only applicable when more than one video files are provided) (default: CPU cores - 1)',
     action='store', 
     default= mp.cpu_count()-1,
     dest='cores')
 
     parser.add_argument('-o', '--output_location',
+    help='Output directory (default: ./)',
     action='store', 
     default= "",
     dest='destination')
 
+    parser.add_argument('-O', '--output_name',
+    help='Output data file name (default: $(INPUT_FILE_NAME)_trajectory.txt)',
+    action='store', 
+    default= "",
+    dest='save_name')
 
     args = parser.parse_args()
-
 
 
     #generate work list
@@ -67,26 +75,26 @@ def main():
     while(len(video_queue) is not 0):
         vid = video_queue.pop()
         save_destination = os.getcwd() + '/' + args.destination
-        (directory, name) = os.path.split(vid)
-        name = os.path.splitext(name)[0]
-        parameters.append((vid, save_destination + ''  + name + "_trajectory.txt", args.batch_mode))
+        if args.save_name == "":
+            (directory, name) = os.path.split(vid)
+            name = os.path.splitext(name)[0] + "_trajectory.txt"
+        else:
+            name = args.save_name
+        parameters.append((vid, save_destination  + name, args.batch_mode))
     
     #create worker pool and distribute jobs
     pool = mp.Pool(processes = args.cores)
 
-
-    for _ in tqdm.tqdm(pool.imap_unordered(tb.track, parameters), total=len(parameters)):
-        pass
-
+    #execute with overall progress bar
+    if(len(parameters)>1):
+        for _ in tqdm.tqdm(pool.imap_unordered(tb.track, parameters), total=len(parameters)):
+            pass
+    #execute with file progress bar
+    else:
+        pool.imap_unordered(tb.track, parameters)
     
     pool.close()
     pool.join()
-
-    
-
-            
-
-
 
 if __name__ == "__main__":
     main()
