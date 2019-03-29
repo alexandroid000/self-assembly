b0212dc749b8fd4291d7d5516570ab8b9820a9df
Alli Nilles <anilles137@gmail.com>
2019-03-28 19:15:30 -0500

    test_examples: remove old commented tests

--- ParticleSimulator/test/test_examples.py
+++ ParticleSimulator/test/test_examples.py
@@ -46,36 +46,3 @@ class TestExamples(unittest.TestCase):
 #        self.assertTrue(check_grid_topo(env1))
 #        self.assertTrue(check_grid_topo(env2))
 #
-#    def test_simple_step_gen(self):
-#        ns = neighbors(3, env2)
-#        self.assertSequenceEqual(ns, [5,2])
-#        p1 = gridworld_step_prob(3,5, types2[0], env2)
-#        self.assertEqual(p1, 0.2)
-#        p2 = gridworld_step_prob(3,2, types2[1], env2)
-#        self.assertEqual(p2, 0.3)
-#
-#    def test_transitions(self):
-#        output = np.array([[[[0.33333333, 0.33333333, 0.        , 0.        , 0.33333333, 0.  ],
-#                           [0.25      , 0.25      , 0.25      , 0.        , 0.25 , 0.      ],
-#                           [0.        , 0.33333333, 0.33333333, 0.33333333, 0. , 0.        ],
-#                           [0. , 0.  , 0.33333333, 0.33333333, 0.        , 0.33333333],
-#                           [0.33333333, 0.33333333, 0.        , 0.        , 0.33333333, 0.        ],
-#                           [0.        , 0.        , 0.        , 0.5       , 0.        , 0.5       ]]],
-#                       [[[0.29411765, 0.35294118, 0. , 0.        , 0.35294118, 0. ],
-#                           [0.26086957, 0.2173913 , 0.26086957, 0.        , 0.26086957, 0.        ],
-#                           [0.        , 0.35294118, 0.29411765, 0.35294118, 0.        , 0.        ],
-#                           [0.        , 0.        , 0.35294118, 0.29411765, 0.        , 0.35294118],
-#                           [0.5       , 0.08333333, 0.        , 0.        , 0.41666667, 0.        ],
-#                           [0.  , 0.        , 0.        , 0.16666667, 0.        , 0.83333333]]]])
-#        calcd =  mkTransitions(env2, types2, 1, len(env2), gridworld_step_prob)
-#        np.testing.assert_almost_equal(calcd, output, decimal=7, verbose=True)
-#
-#    def test_encode_decode(self):
-#        N = randint(2,10)
-#        X = randint(5,10)
-#        states = np.random.choice(np.arange(X, dtype=np.uint64), N)
-#        state_int = encodeJointState(states, X)
-#        decoded = decodeJointState(state_int, N, X)
-#        np.testing.assert_almost_equal(states, decoded, decimal=7, verbose=True)
-#
-#
