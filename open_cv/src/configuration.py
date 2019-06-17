03a3caadac2086f455c158a56e43a3da1c04ee44
Chris Horn <cchorn212@gmail.com>
2019-06-16 23:37:22 -0700

    Updated configuration comment

--- open_cv/src/configuration.py
+++ open_cv/src/configuration.py
@@ -1,3 +1,8 @@
+# file for loading different constants and parameters
+
+# NOTE: currently not implemeneted, only used when
+#       built into a module
+
 class ExtractorConfiguration(object):
     STARTING_MIN_RAD = 10
     STARTING_MAX_RAD = 20
